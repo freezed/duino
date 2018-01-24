ad44bcc316146153f5a79cdbdb4e9f1d4dc3faee
Fred Z <freezed@users.noreply.github.com>
2018-01-25 00:22:13 +0100

    Blink-test a 2 led pour OLIMEXINO-32u4|test sur AVR-T32U4:FAIL

---
+++ Final_Shipping_Blink/Final_Shipping_Blink.ino
@@ -0,0 +1,28 @@
+/*
+ 
+ Alternatively Blinking LED1 and LED2 for OLIMEXINO-32u4
+ 
+ */
+ 
+//OLIMEXINO-32u4 has two general purpose LEDs at ARDUINO pins 7 and 9
+int led1 = 7;
+int led2 = 9;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+
+// initialize the digital pins as an outputs.
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);   
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);               // wait for 200 ms
+  digitalWrite(led1, LOW);
+  delay(200);
+  digitalWrite(led2, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(200); // wait for 200ms
+  digitalWrite(led2, LOW);  
+}
