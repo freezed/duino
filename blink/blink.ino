3a61bd055e88afe9d3d2f2586a1b6dde9167dbd2
Fred Z <freezed@users.noreply.github.com>
2018-01-25 03:21:57 +0100

    Blink example, avec le bon LEDpin|AVR-T32U4:OK

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,30 +1,22 @@
 /*
  *   Blink
- *   Turns on an LED on for one second, then off for one second, repeatedly.
- *
- *   This example code is in the public domain.
+ *   Turns on an LED on for, then off, repeatedly.
  *
  *   Modified to blink the second led on Olimexino-T32u4
  */
 
 // Useful definitions for the two LEDs on the board
-#define GLED 7      // The GREEN  LED is on Pin 7
-#define YLED 9      // The YELLOW LED is on Pin 9
+#define YLED 17      // The YELLOW LED is on Pin 17
+#define TIME_DELAY 500
 
 void setup() {
     // initialize the digital pin as an output.
-    pinMode(GLED, OUTPUT);
     pinMode(YLED, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(GLED, HIGH);   // set the LED on
-    delay(500);                 // wait for a second
-    digitalWrite(GLED, LOW);    // set the LED off
-    delay(500);                 // wait for a second
-
     digitalWrite(YLED, HIGH);
-    delay(500);
+    delay(TIME_DELAY);
     digitalWrite(YLED, LOW);
-    delay(500);
+    delay(TIME_DELAY);
 }
